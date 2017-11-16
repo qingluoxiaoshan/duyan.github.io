d99d779fa0dd333cef37d01456449b16a48a4c69
liugen <ly954913771@163.com>
2017-11-16 22:15:52 +0800

    ios

--- js/imageTransform3D.js
+++ js/imageTransform3D.js
@@ -9,6 +9,23 @@
 
 "use strict";
 
+function audioAutoPlay(id){
+	var audio = document.getElementById(id),
+		play = function(){
+			audio.play();
+			document.removeEventListener("touchstart",play, false);
+		};
+	audio.play();
+	document.addEventListener("WeixinJSBridgeReady", function () {
+		play();
+	}, false);
+	document.addEventListener('YixinJSBridgeReady', function() {
+		play();
+	}, false);
+	document.addEventListener("touchstart",play, false);
+}
+audioAutoPlay('music');
+
 var ge1doot = ge1doot || {};
 ge1doot.transform3D = {};
 
